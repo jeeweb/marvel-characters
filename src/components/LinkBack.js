46f41f21da710a3bf104430911e9ee1eebf0d22e
jeeweb <52396673+jeeweb@users.noreply.github.com>
2024-02-26 20:57:04 +0900

    feat: Back button component

---
+++ src/components/LinkBack.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import styles from "./LinkBack.module.css";
+
+function LinkBack() {
+  return (
+    <Link to={"/"} className={styles.link_back}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="1.6em"
+        height="1.6em"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="#EA1D24"
+          d="M19 11H7.83l4.88-4.88c.39-.39.39-1.03 0-1.42a.996.996 0 0 0-1.41 0l-6.59 6.59a.996.996 0 0 0 0 1.41l6.59 6.59a.996.996 0 1 0 1.41-1.41L7.83 13H19c.55 0 1-.45 1-1s-.45-1-1-1"
+        ></path>
+      </svg>
+      BACK TO LIST
+    </Link>
+  );
+}
+
+export default LinkBack;
